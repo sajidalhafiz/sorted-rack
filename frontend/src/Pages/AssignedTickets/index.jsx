005e1696bb019ba73ffffbb15693f5364ec60728
tryit-100 <safwan.std21@gmail.com>
2024-10-10 11:10:54 +0600

    assignedTicket page implemented

--- frontend/src/Pages/AssignedTickets/index.jsx
+++ frontend/src/Pages/AssignedTickets/index.jsx
@@ -1,28 +1,28 @@
-import React, { useRef, useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { BsGear } from 'react-icons/bs';
 import { BiCheckCircle } from "react-icons/bi";
 import { Form, Table, Toast, Container, Dropdown, Col } from "react-bootstrap";
 import { axiosSecure } from "../../api/axios";
 import PaginationComponent from "../../component/Pagination/Pagination";
-import Columns from "../../constants/AssigmentColumns.json";
+import Columns from "../../constants/AssignedTicketColumns.json";
 
-const AssignItem = () => {
+const AssignedTickets = () => {
   const [columns, setColumns] = useState(Columns);
-  const [assignedDeviceUserList, setAssignedDeviceUserList] = useState([]);
+  const [assignedTicketsList, setAssignedTicketsList] = useState([]);
   const [search, setSearch] = useState("");
   const [totalItems, setTotalItems] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const ITEMS_PER_PAGE = 10;
   const [showToaster, setShowToaster] = useState(false);
 
-  const getAssignedDeviceDetails = async () => {
-    const response = await axiosSecure.get("/assignedProduct", {
+  const getAssignedTicketDetails = async () => {
+    const response = await axiosSecure.get("/assigned-tickets", {
       headers: {
         Authorization: `Bearer ${localStorage.userDetails && JSON.parse(localStorage.userDetails).token}`,
       },
     });
 
-    setAssignedDeviceUserList(response?.data?.assignedDevices);
+    setAssignedTicketsList(response?.data?.assignedTickets);
   };
 
   const getDate = (date) => {
@@ -33,13 +33,11 @@ const AssignItem = () => {
     return `${dt}-${month}-${year}`;
   };
 
-  const handleUnassignment = async (assignedDeviceDocId) => {
+  const handleUnassignment = async (assignedTicketId) => {
     try {
       const response = await axiosSecure.patch(
-        `assignedProduct/${assignedDeviceDocId}`,
-        {
-          product: assignedDeviceDocId,
-        },
+        `assigned-tickets/${assignedTicketId}`,
+        {},
         {
           headers: {
             Authorization: `Bearer ${localStorage.userDetails && JSON.parse(localStorage.userDetails).token}`,
@@ -48,7 +46,7 @@ const AssignItem = () => {
       );
       if (response?.status === 200) {
         setShowToaster(true);
-        getAssignedDeviceDetails();
+        getAssignedTicketDetails();
       }
     } catch (err) {
       alert(err);
@@ -56,7 +54,7 @@ const AssignItem = () => {
   };
 
   useEffect(() => {
-    getAssignedDeviceDetails();
+    getAssignedTicketDetails();
   }, []);
 
   const handleSearch = (e) => {
@@ -64,17 +62,19 @@ const AssignItem = () => {
   };
 
   const filtered = useMemo(() => {
-    let filteredResult = assignedDeviceUserList;
+    let filteredResult = assignedTicketsList;
     setTotalItems(filteredResult?.length);
-    console.log(filteredResult)
     if (search) {
-      filteredResult = filteredResult.filter((result) => result.firstName.toLowerCase().includes(search.toLowerCase()));
+      filteredResult = filteredResult.filter((result) => 
+        result.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        result.ticketTitle.toLowerCase().includes(search.toLowerCase())
+      );
     }
     return filteredResult?.slice(
       (currentPage - 1) * ITEMS_PER_PAGE,
       (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
     );
-  }, [currentPage, assignedDeviceUserList, search]);
+  }, [currentPage, assignedTicketsList, search]);
 
   const handlerCheckbox = (e) => {
     const checkboxStatus = e.target.checked;
@@ -93,10 +93,10 @@ const AssignItem = () => {
     <Container>
       <div className="d-flex align-items-center justify-content-between">
         <div className="col-9">
-          <h2 className="py-3">Assigned Devices</h2>
+          <h2 className="py-3">Assigned Tickets</h2>
         </div>
         <Form.Group as={Col} md="3" className="pe-3" controlId="validationCustom01">
-          <Form.Control onChange={handleSearch} type="text" placeholder="Search devices by first name" />
+          <Form.Control onChange={handleSearch} type="text" placeholder="Search tickets by name or title" />
         </Form.Group>
       </div>
 
@@ -129,7 +129,7 @@ const AssignItem = () => {
             &nbsp;
           </div>
           <div className="toaster-title">
-            <strong className="me-auto">Unassignment Successfull</strong>
+            <strong className="me-auto">Unassignment Successful</strong>
           </div>
         </Toast.Header>
       </Toast>
@@ -153,7 +153,7 @@ const AssignItem = () => {
                 <tr key={index}>
                   {columns.length > 0  && columns.map(({ name, show }) => (
                     <td id={name} className={`${show ? "show" : "hide"} `}>
-                      {item[name] || "---"}
+                      {name === "dueDate" ? getDate(item[name]) : (item[name] || "---")}
                     </td>
                   ))}
                   <td id="actions" className="text-center">
@@ -169,7 +169,7 @@ const AssignItem = () => {
           </tbody>
         </Table>
       ) : (
-        <h4 className="ms-3 mt-3">no devices found....</h4>
+        <h4 className="ms-3 mt-3">No assigned tickets found...</h4>
       )}
       <div className="d-flex justify-content-end me-3">
         <PaginationComponent
@@ -183,4 +183,4 @@ const AssignItem = () => {
   );
 };
 
-export default AssignItem;
+export default AssignedTickets;
